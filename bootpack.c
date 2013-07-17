b958410bc1d75f4866cb3167709711f5d4918d67
Jinsheng Shen <scmbsaj@gmail.com>
2013-07-18 01:21:05 +0800

    analysis mouse data

--- bootpack.c
+++ bootpack.c
@@ -5,8 +5,16 @@
 #include "bootpack.h"
 #include <stdio.h>
 
+
+struct MOUSE_DEC {
+	unsigned char buf[3], phase;
+};
+
 extern struct FIFO8 keyfifo;
 extern struct FIFO8 mousefifo;
+void enable_mouse(struct MOUSE_DEC *mdec);
+void init_keyboard(void);
+int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat);	//
 
 void HariMain(void)
 {
@@ -14,6 +22,7 @@ void HariMain(void)
 	struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
 	char s[40], mcursor[256], keybuf[32], mousebuf[128];
 	int mx, my, i;
+	struct MOUSE_DEC mdec;
 	
 	
 	init_gdtidt();
@@ -36,7 +45,7 @@ void HariMain(void)
 	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
 
 
-	enable_mouse();
+	enable_mouse(&mdec);
 
 	for (;;) {
 		io_cli();
@@ -52,9 +61,12 @@ void HariMain(void)
 			} else if (fifo8_status(&mousefifo) != 0) {
 				i = fifo8_get(&mousefifo);
 				io_sti();
-				sprintf(s, "%02X", i);
-				boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 47, 31);
-				putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
+				if (mouse_decode(&mdec, i) != 0) {
+					/* 获得全部3字节数据，显示数据 */
+					sprintf(s, "%02X %02X %02X", mdec.buf[0], mdec.buf[1], mdec.buf[2]);
+					boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 32 + 8 * 8 - 1, 31);
+					putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
+				}
 			}
 		}
 	}
@@ -91,12 +103,44 @@ void init_keyboard(void)
 #define KEYCMD_SENDTO_MOUSE		0xd4 	//发送数据给鼠标
 #define MOUSECMD_ENABLE			0xf4 	//鼠标答复信息
 
-void enable_mouse(void)
+void enable_mouse(struct MOUSE_DEC *mdec)
 {
 	/* 激活鼠标 */
 	wait_KBC_sendready();
 	io_out8(PORT_KEYCMD, KEYCMD_SENDTO_MOUSE);
 	wait_KBC_sendready();
 	io_out8(PORT_KEYDAT, MOUSECMD_ENABLE);
+
+	mdec->phase = 0; /* 等待0xfa的阶段 */
 	return; 
+}
+
+int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat)
+{
+	if (mdec->phase == 0) {
+		/* 等待鼠标0xfa的阶段 */
+		if (dat == 0xfa) {
+			mdec->phase = 1;
+		}
+		return 0;
+	}
+	if (mdec->phase == 1) {
+		/* 等待鼠标的第1字节数据 */
+		mdec->buf[0] = dat;
+		mdec->phase = 2;
+		return 0;
+	}
+	if (mdec->phase == 2) {
+		/* 等待鼠标的第2字节数据 */
+		mdec->buf[1] = dat;
+		mdec->phase = 3;
+		return 0;
+	}
+	if (mdec->phase == 3) {
+		/* 等待鼠标的第3字节数据 */
+		mdec->buf[2] = dat;
+		mdec->phase = 1;
+		return 1;
+	}
+	return -1; /* 异常返回 */
 }
