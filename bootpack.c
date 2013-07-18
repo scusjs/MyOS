375afff1faee6c53c3a219a8facb6647dcc4321c
Jinsheng Shen <scmbsaj@gmail.com>
2013-07-18 13:38:18 +0800

    move mouse

--- bootpack.c
+++ bootpack.c
@@ -8,6 +8,7 @@
 
 struct MOUSE_DEC {
 	unsigned char buf[3], phase;
+	int x, y, btn;	//x,y表示鼠标移动信息，btn表示按键信息
 };
 
 extern struct FIFO8 keyfifo;
@@ -63,9 +64,43 @@ void HariMain(void)
 				io_sti();
 				if (mouse_decode(&mdec, i) != 0) {
 					/* 获得全部3字节数据，显示数据 */
-					sprintf(s, "%02X %02X %02X", mdec.buf[0], mdec.buf[1], mdec.buf[2]);
-					boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 32 + 8 * 8 - 1, 31);
+					sprintf(s, "[lcr %4d %4d]", mdec.x, mdec.y);
+					if ((mdec.btn & 0x01) != 0) {
+						s[1] = 'L';
+					}
+					if ((mdec.btn & 0x02) != 0) {
+						s[3] = 'R';
+					}
+					if ((mdec.btn & 0x04) != 0) {
+						s[2] = 'C';
+					}
+					boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 32 + 15 * 8 - 1, 31);
 					putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
+
+					/*移动鼠标指针*/
+					boxfill8(binfo->vram, binfo->scrnx, COL8_008484, mx, my, mx + 15, my + 15); /* 隐藏原来的鼠标指针 */
+
+
+					mx += mdec.x;
+					my += mdec.y;
+					if (mx < 0) {
+						mx = 0;
+					}
+					if (my < 0) {
+						my = 0;
+					}
+
+
+					if (mx > binfo->scrnx - 16) {
+						mx = binfo->scrnx - 16;
+					}
+					if (my > binfo->scrny - 16) {
+						my = binfo->scrny - 16;
+					}
+					sprintf(s, "(%3d, %3d)", mx, my);
+					boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 0, 0, 79, 15); /* 隐藏坐标 */
+					putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s); /* 显示坐标 */
+					putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16); /* 重新绘制鼠标 */
 				}
 			}
 		}
@@ -126,8 +161,11 @@ int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat)
 	}
 	if (mdec->phase == 1) {
 		/* 等待鼠标的第1字节数据 */
-		mdec->buf[0] = dat;
-		mdec->phase = 2;
+		if ((dat & 0xc8)==0x08) 	//修正鼠标按键信息
+		{
+			mdec->buf[0] = dat;
+			mdec->phase = 2;
+		}
 		return 0;
 	}
 	if (mdec->phase == 2) {
@@ -140,6 +178,19 @@ int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat)
 		/* 等待鼠标的第3字节数据 */
 		mdec->buf[2] = dat;
 		mdec->phase = 1;
+
+		mdec->btn = mdec->buf[0] & 0x07; 	//提取按键信息
+		mdec->x = mdec->buf[1];
+		mdec->y = mdec->buf[2];
+		if ((mdec->buf[0] & 0x10) != 0) {	//提取水平移动信息
+			mdec->x |= 0xffffff00;
+		}
+		if ((mdec->buf[0] & 0x20) != 0) {	//提取竖直移动信息
+			mdec->y |= 0xffffff00;
+		}
+		mdec->y = - mdec->y; /* 鼠标移动的y轴方向与屏幕y轴方向相反 */
+
+
 		return 1;
 	}
 	return -1; /* 异常返回 */
